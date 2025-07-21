e9ba8314332462a32f47da68b6c2ece25ea9748b
jefe-git <jeffreed4@icloud.com>
2025-07-21 02:24:57 -0500

    assingment changes

---
+++ mixedTypeArraysPractice/mixedArrayPrac/mixedTypeArrays.js
@@ -0,0 +1,29 @@
+//Task 1
+
+let profileArray = {
+  userName: "Johny Cox",
+  age: 25,
+  subscriptionStatus: "Active",
+  location: "Saint Louis",
+  hobbies: ["Video Games", "Cars"]
+};
+
+//Task 2 
+
+console.log("User Name:", profileArray.userName);
+console.log("Second Hobby:" , profileArray.hobbies[1]);
+
+// Task 3
+
+profileArray.age = 27
+profileArray.hobbies.push("skating");
+
+// Task 4 
+
+console.log("Updated Age:", profileArray.age);
+console.log("Updated Hobbies:", profileArray.hobbies);
+
+//End of Assingment
+
+
+
